e1458aa7abbadd34ecf4f58394abccb07f65c035
mertyasarerkoc <mert.erkoc@finkoto.com>
2023-08-13 00:39:06 +0300

    mongo pool and very good analysis added

--- src/core/dio/model/response_entity.dart
+++ src/core/dio/model/response_entity.dart
@@ -2,12 +2,15 @@ class ResponseEntity<T> {
   ResponseEntity({required this.statusCode, this.message, this.data});
 
   ResponseEntity.fromJson(
-      Map<String, dynamic> json, this.statusCode, this.message, this.data) {
+    Map<String, dynamic> json,
+    this.statusCode,
+    this.message,
+    this.data,
+  ) {
     statusCode = json['code'] as int;
     message = json['message'] != null ? json['message'] as String : null;
     data = json['data'] != null ? json['data'] as T : null;
   }
-
 
   late final int statusCode;
   late final String? message;
