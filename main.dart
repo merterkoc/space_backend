0bf665913deb7937531d69d2f9ffff65f48c7bbd
mertyasarerkoc <mertyasarerkocc@gmail.com>
2023-09-19 21:46:07 +0300

    Custom init method added

--- main.dart
+++ main.dart
@@ -5,11 +5,15 @@ import 'package:mongo_pool/mongo_pool.dart';
 import 'package:space_backend/src/core/logger/logger.dart';
 import 'package:space_backend/src/env/platform_environment.dart';
 
-Future<HttpServer> run(Handler handler, InternetAddress ip, int port) async {
+Future<void> init(InternetAddress ip, int port) async {
   final ip = InternetAddress.anyIPv4;
   final port = int.tryParse(PlatformEnvironment.port ?? '8080') ?? 8080;
-  logger.i('Server started on port $port');
+  logger.i('Server started on port $port, ip $ip');
   await initializeMongo();
+}
+
+
+Future<HttpServer> run(Handler handler, InternetAddress ip, int port) async {
   return serve(handler, ip, port);
 }
 
