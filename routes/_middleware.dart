46f7a940c530b12f2b9955968092c617fdc69517
mertyasarerkoc <mertyasarerkocc@gmail.com>
2023-12-16 03:10:40 +0300

    Handling Cross-Origin Resource Sharing (CORS)

--- routes/_middleware.dart
+++ routes/_middleware.dart
@@ -1,5 +1,16 @@
 import 'package:dart_frog/dart_frog.dart';
+import 'package:shelf_cors_headers/shelf_cors_headers.dart' as shelf;
 
 Handler middleware(Handler handler) {
-  return handler.use(requestLogger());
+  return handler.use(requestLogger()).use(
+        fromShelfMiddleware(
+          shelf.corsHeaders(
+            headers: {
+              ///Handling Cross-Origin Resource Sharing (CORS)
+              ///https://dartfrog.vgv.dev/docs/advanced/cors
+              shelf.ACCESS_CONTROL_ALLOW_ORIGIN: 'https://space-3243a.web.app',
+            },
+          ),
+        ),
+      );
 }
