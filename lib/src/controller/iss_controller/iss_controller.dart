2f699224ed6ad9205b814d4d797c58dfbccb31dd
mertyasarerkoc <mertyasarerkocc@gmail.com>
2024-01-21 00:30:29 +0300

    ISS feature added.

---
+++ lib/src/controller/iss_controller/iss_controller.dart
@@ -0,0 +1,20 @@
+import 'package:dart_frog/dart_frog.dart';
+import 'package:space_backend/src/service/iss_service/iss_service.dart';
+import 'package:space_backend/src/util/extension/request_extension.dart';
+
+class ISSController {
+  final ISSService _issService = ISSService();
+
+  Future<Response> fetchISSInformation(RequestContext context) async {
+    try {
+      final response = await _issService.getISSCurrentLocation();
+      return Response().ok(
+        body: response,
+      );
+    } on Exception catch (e) {
+      return Response().unknownError(
+        body: e.toString(),
+      );
+    }
+  }
+}
--- lib/src/service/iss_service/iss_service.dart
+++ lib/src/service/iss_service/iss_service.dart
@@ -1,6 +1,9 @@
 import 'dart:async';
 
+import 'package:space_backend/src/core/logger/logger.dart';
 import 'package:space_backend/src/repository/iss_repository.dart';
+import 'package:space_backend/src/service/model/dto/iss_dto/iss_dto.dart';
+import 'package:space_backend/src/service/model/mapper/iss_mapper.dart';
 import 'package:space_backend/src/util/observable.dart';
 
 typedef IssObservable = Observable;
@@ -18,12 +21,14 @@ class ISSService extends IssObservable {
 
   late final ISSRepository _issRepository;
 
-  Future<void> getISSCurrentLocation() async {
+  Future<ISSDTO> getISSCurrentLocation() async {
     try {
       final result = await _issRepository.getISSCurrentLocation();
       notify(result);
+      return ISSMapper().from(result);
     } on Exception {
-      return;
+      logger.e('ISSService.getISSCurrentLocation: Error');
+      rethrow;
     }
   }
 }
