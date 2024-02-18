2100ef024281ca6ef838d99945061b27c59bd706
mertyasarerkoc <mertyasarerkocc@gmail.com>
2024-02-18 21:15:14 +0300

    Swagger added.

--- script/build_run.sh
+++ script/build_run.sh
@@ -3,5 +3,6 @@
 # Take port number from environment variable
 port_number="$PORT"
 dart run build_runner build --delete-conflicting-outputs
+dart pub global activate nenuphar_cli
 # If the port is already in use, kill the process
 lsof -t -i:"$port_number" | xargs kill -9
